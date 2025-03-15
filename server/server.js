16cb7967dfc37e35449e123ecc7c642544e7c1f2
Harsh Poojary <harshpoojary@Harshs-MacBook-Air.local>
2025-03-15 19:57:32 +0530

    changes in code

--- server/server.js
+++ server/server.js
@@ -1,22 +1,32 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const app = express()
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import userRoutes from './routes/userRoutes.js';
+import priceRoutes from './routes/priceRoutes.js';
 
+const app = express();
+const FRONTEND_ORIGIN = 'http://localhost:3000'; // Update with your frontend URL
+
+// Middlewares
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: FRONTEND_ORIGIN,
+    credentials: true, // Allow cookies to be sent
+  })
+);
 
+// Routes
+app.use('/api/user', userRoutes);
+app.use('/api/price', priceRoutes);
 
-//routes
-const userRoutes = require('./routes/userRoutes');
-const priceRoutes = require("./routes/priceRoutes")
-
-
-const port = 3000
-// app.use("/",userRoutes)
+// Test Route
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.use("/api/price",priceRoutes)
+  res.send('Hello World!');
+});
 
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
