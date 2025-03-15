4527d5a248358e710e8d5d5b3d035f5b10f6a320
Harsh Poojary <harshpoojary@Harshs-MacBook-Air.local>
2025-03-15 23:43:19 +0530

    change in serber.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: FRONTEND_ORIGIN,
+    origin: true,
     credentials: true, // Allow cookies to be sent
   })
 );
